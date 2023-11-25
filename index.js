de68a8e94d618bf8ed59be9509f5cc1fdfd40a95
Shahriar Ahmmed <shahriarzemam28@gmail.com>
2023-11-25 11:00:29 +0600

    Get method created for all Apartments

--- index.js
+++ index.js
@@ -28,8 +28,14 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
-        
+        const apartCollection = client.db("gulshanDb").collection("aparts")
 
+
+        // Get All Apartments
+        app.get('/aparts',async(req,res)=>{
+            const result = await apartCollection.find().toArray()
+            res.send(result);
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
