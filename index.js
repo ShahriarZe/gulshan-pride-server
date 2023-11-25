c8e3beeaf118a810e6bb5549cdbca38f718a82d3
Shahriar Ahmmed <shahriarzemam28@gmail.com>
2023-11-25 19:46:27 +0600

    Delete method created for deleting specific user

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -43,6 +43,20 @@ async function run() {
                 return res.send({ message: "User already exists", insertedId: null })
             }
             const result = await userCollection.insertOne(user)
+            res.send(result)
+        })
+
+        // Get All Users
+        app.get('/users', async (req, res) => {
+            const result = await userCollection.find().toArray()
+            res.send(result)
+        })
+
+        // Delete Users
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await userCollection.deleteOne(query)
             res.send(result)
         })
 
